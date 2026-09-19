8c1857d9961eb82f03904b4303996060d3b75681
Penedov <163603238+Penedov@users.noreply.github.com>
2026-09-18 23:46:18 -0300

    Salvando meu progresso

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa compilado pelo CMD!");
+	System.out.println("Alterando o codigo fonte!");
+    }
+}
